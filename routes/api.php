240c10b5cc4a279a1ba05e812472b5578f49a2cc
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-11 13:06:50 +0000

    api routes updated

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\RentalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/cars', CarController::class);
+    Route::post('/rentals', [RentalController::class, 'store']);
+    Route::get('/rentals/success', [RentalController::class, 'success'])->name('rentals.success');
+    Route::get('/rentals/cancel', [RentalController::class, 'cancel'])->name('rentals.cancel');
+});
