463a8aff092742c8e07524b958a6529fb9be137b
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-11 13:03:19 +0000

    rental migration updated

--- database/migrations/2025_03_10_130033_create_rentals_table.php
+++ database/migrations/2025_03_10_130033_create_rentals_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('rentals', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('car_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount',8,2);
+            $table->date('start_date');
+            $table->date('return_date');
             $table->timestamps();
         });
     }
