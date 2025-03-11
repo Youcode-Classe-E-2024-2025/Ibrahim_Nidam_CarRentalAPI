e69959574702996725d1d4d822680c47697acdaa
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-11 12:48:51 +0000

    payment model updated

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
     /** @use HasFactory<\Database\Factories\PaymentFactory> */
     use HasFactory;
+
+    protected $fillable = ['amount', 'rental_id'];
+
+    public function rental(){
+        return $this->BelongsTo(Rental::class);
+    }
 }
