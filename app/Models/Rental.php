263470be2a1e7d0a3ebe47bf487bf41ea7c61af4
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-11 12:49:00 +0000

    rental model updated

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -9,4 +9,19 @@ class Rental extends Model
 {
     /** @use HasFactory<\Database\Factories\RentalFactory> */
     use HasFactory;
+
+    protected $fillable = ['user_id', 'car_id','amount', 'start_date', 'return_date'];
+
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function cars(){
+        return $this->belongsTo(Car::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
 }
