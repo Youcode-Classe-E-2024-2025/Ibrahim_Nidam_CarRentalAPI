eb7344ad682b7ab3489b8358388193e84e6f31ee
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-11 12:59:17 +0000

    swagger downloaded

---
+++ app/Swagger/OpenApiDefinitions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Info(
+ *     title="Car Rental API",
+ *     version="1.0.0",
+ *     description="API documentation for the Car Rental application",
+ *     @OA\Contact(
+ *         email="support@carrental.com"
+ *     )
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer"
+ * )
+ */
+
+class OpenApiDefinitions
+{
+}
