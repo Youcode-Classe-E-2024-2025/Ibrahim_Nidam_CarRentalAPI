1ec359c09c2c61bc0afc99d10ff81cc2743487ec
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-11 13:01:58 +0000

    car migration updated

--- database/migrations/2025_03_10_130026_create_cars_table.php
+++ database/migrations/2025_03_10_130026_create_cars_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('make');
+            $table->string('model');
+            $table->year('year');
             $table->timestamps();
         });
     }
